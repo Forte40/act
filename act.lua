b9cf1a15d55ed13add0a1e811a92a48c63aa07ac
Forte40 <scott.zeroviz.us@gmail.com>
2013-08-14 18:49:36 -0700

    change compareTo to 'Ct' and added 'c' as turtle.craft

--- act.lua
+++ act.lua
@@ -512,8 +512,8 @@ lang.locationaction = C(
   S("[Gw]", "<", lang.token, M(S(",", lang.token, P{2})), ">", P({waypointtype=1, waypoint=3, priority=4}))
 )
 lang.param2action = S("t", lang.int, ",", lang.int, P{action=1, param1=2, param2=4}) 
-lang.paramaction = S(C("[cegostz]", "[E][fud]"), C(lang.number, lang.string), P{action=1, param=2})
-lang.simpleaction = C("[efblrudq]", "[ABCDEGHMPS][fud]", "Gb", "I[csf]")
+lang.paramaction = S(C("[egostz]", "[E][fud]", "Ct"), C(lang.number, lang.string, lang.boolvar), P{action=1, param=2})
+lang.simpleaction = C("[cefblrudq]", "[ABCDEGHMPS][fud]", "Gb", "I[csf]")
 lang.extension = S("%%", lang.token, Z(S(",", C(lang.token, lang.string, lang.number), P{2})), "%%", P{extension=2, params=3})
 lang.action = {}
 lang.joiner = S("/", O(lang.action))
@@ -766,6 +766,7 @@ local tHandlers = {
   ["t"] = turtle.transferTo,
   ["e"] = turtle.refuel,
   ["o"] = io.write,
+  ["c"] = turtle.craft,
   -- dig
   ["Df"] = turtle.dig,
   ["Du"] = turtle.digUp,
@@ -807,7 +808,7 @@ local tHandlers = {
   ["Cf"] = turtle.compare,
   ["Cu"] = turtle.compareUp,
   ["Cd"] = turtle.compareDown,
-  ["c"] = turtle.compareTo,
+  ["Ct"] = turtle.compareTo,
   -- inspect
   ["Ic"] = turtle.getItemCount,
   ["Is"] = turtle.getItemSpace,
